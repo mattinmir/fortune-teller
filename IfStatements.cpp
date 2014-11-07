98ecab3aa1ccd20478cfaf80f13d7ef164456a03
mattinmir <mattinmir@users.noreply.github.com>
2014-11-07 15:10:13 +0000

    Create IfStatements.cpp

---
+++ IfStatements.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int my_variable = 10;
+
+	if (my_variable == 10)
+	{
+		cout << "my_variable is 10!" << endl;
+	}
+
+	if (my_variable < 5)
+	{
+		cout << "my_variable is less than 5!" << endl;
+	}
+
+	if (my_variable > 20)
+	{
+		cout << "my_variable is greater than 20!" << endl;
+	}
+
+	if (my_variable != 100)
+	{
+		cout << "my_variable is not 100!" << endl;
+	}
+}
