b73c5daa813d0c773fa66486be63cc70740b479d
mattinmir <mattinmir@users.noreply.github.com>
2014-11-07 15:34:32 +0000

    Create Math.cpp

---
+++ Math.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float number;
+
+	number = 10;
+	cout << number + 5 << endl;
+
+	number = 20;
+	cout << number - 7 << endl;
+
+	number = 2;
+	cout << number * 3 << endl;
+
+	number = 5;
+	cout << number / 2 << endl;
+
+}
