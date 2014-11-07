11694dc1e824f4d7aa19e2883a4ee210ac9ae8d5
mattinmir <mattinmir@users.noreply.github.com>
2014-11-07 15:23:50 +0000

    Create Input.cpp

---
+++ Input.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string animal;
+
+	cout << "Enter your favourite animal" << endl;
+	cin >> animal;
+
+	cout << "Your favourite animal is: " << animal;
+
+	int age;
+
+	cout << "Enter your age" << endl;
+	cin >> age;
+
+	cout << "In two years, you will be" << age + 2 << "years old" << endl;
+
+}
