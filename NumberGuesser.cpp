4c88e59462fbde77ee0e55452953c15b34f565d7
mattinmir <mattinmir@users.noreply.github.com>
2014-11-08 16:19:12 +0000

    Create NumberGuesser.cpp

---
+++ NumberGuesser.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int secretNumber = 150;
+	int guess = 0;
+	while (guess != secretNumber)
+	{
+		cout << "Make a Guess!" << endl;
+		cin >> guess;
+
+		if (guess < secretNumber)
+			cout << "Too low!" << endl;
+		else if (guess < secretNumber)
+			cout << "Too low!" << endl;
+		else if (guess == secretNumber)
+			cout << "You win!" << endl;
+
+	}
+}
